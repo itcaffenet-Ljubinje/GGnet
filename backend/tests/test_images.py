08ce5e7934ae708b623a8e822c84a5ef8fa8b6a7
itcaffenet-Ljubinje <itcaffenet@gmail.com>
2025-10-03 19:00:40 +0200

    Update test_images.py

--- backend/tests/test_images.py
+++ backend/tests/test_images.py
@@ -1,9 +1,7 @@
 """
 Test image management endpoints
 """
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 
 import pytest
 from httpx import AsyncClient
