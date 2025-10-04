564b4e377c7065cb462b958bf3700cc47069b87a
itcaffenet-Ljubinje <itcaffenet@gmail.com>
2025-10-04 02:35:31 +0200

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,12 +2,11 @@
 # app/models/__init__.py
 
 # Import glavnih modela
-from .user import User, UserRole, UserStatus
+from .user import User
 from .image import Image
 from .machine import Machine
 from .session import Session  # ako imaš model za sesije
 from .target import Target    # ako imaš target model
-from .game import Game        # ako imaš game model
 
 # Dodaj sve ostale modele ovde
 # from .other_model import OtherModel
@@ -15,13 +14,10 @@ from .game import Game        # ako imaš game model
 # Napravi __all__ za lakši import
 __all__ = [
     "User",
-    "UserRole",
-    "UserStatus",
     "Image",
     "Machine",
     "Session",
     "Target",
-    "Game",
     # dodaj ostale modele ovde
 ]
 
