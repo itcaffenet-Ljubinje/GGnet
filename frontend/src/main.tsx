cd00b2ff848f991a4848cad6bcb3f4082a489305
itcaffe net <itcaffenet@gmail.com>
2025-10-07 12:44:58 +0200

    fix: import StrictMode from react in main.tsx
    Fixed TypeScript errors by importing StrictMode instead of using React.StrictMode:
    - Added 'import { StrictMode } from react'
    - Replaced <React.StrictMode> with <StrictMode>
    - Resolves TS2686: 'React' refers to a UMD global error
    
    This allows TypeScript to compile without treating React as a global variable.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -17,7 +18,7 @@ const queryClient = new QueryClient({
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter
         future={{
@@ -54,6 +55,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         />
       </BrowserRouter>
     </QueryClientProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
 
